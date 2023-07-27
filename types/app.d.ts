c9cfd97b6f34375d9544be54582d93f08cc432e9
Lucas Doell <lmd248@cornell.edu>
2023-07-27 00:58:35 -0400

    add env variables typing

--- types/app.d.ts
+++ types/app.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="nativewind/types" />
+
+import { z } from "zod";
+
+const envVariables = z.object({
+  PLAID_CLIENT_ID: z.string(),
+  PLAID_SECRET: z.string(),
+});
+
+envVariables.parse(process.env);
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envVariables> {}
+  }
+}
